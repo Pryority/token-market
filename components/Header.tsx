89c4f8c03d75f2f7cd27d55ce1b9b3082f8d69e3
pryority <matthewapryor@gmail.com>
2022-11-29 13:49:43 -0500

    add nav bar component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,47 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAddress, useMetamask, useDisconnect } from "@thirdweb-dev/react";
+import { useRouter } from "next/router";
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+import tmLogo from "/tm-logo.png";
 
 export default function Header() {
-  // Helpful thirdweb hooks to connect and manage the wallet from metamask.
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
-
+  const tabs = [
+    { tabName: "CREATE", pageName: "/create" },
+    { tabName: "MINT", pageName: "/mint" },
+  ];
+  const { pathname } = useRouter();
   return (
-    <div className={"flex items-center w-full justify-between p-6"}>
+    // <div className={"flex items-center w-full justify-between p-6"}>
+    <div className="flex w-full p-6 justify-between items-center">
       <div className={"flex"}>
         <div>
           <Link href="/" passHref role="button">
-            <img
-              src={"/logo.png"}
-              alt="Thirdweb Logo"
-              width={135}
-              style={{ cursor: "pointer" }}
-            />
+            <div className="h-24 w-24 flex relative">
+              <Image
+                src={tmLogo}
+                alt="Token Market Logo"
+                layout="fill"
+              />
+            </div>
           </Link>
         </div>
       </div>
-      <div className={"flex items-center"}>
-        <ConnectButton/>
+      <div className="flex">
+        {tabs.map((tab) => (
+          <Link href={tab.pageName} key={tab.tabName}>
+            <h2
+              className={`font-semibold px-4 py-1 items-center cursor-pointer hover:bg-gradient-to-b hover:from-yellow-400  hover:bg-[length:50%_2px] hover:bg-no-repeat hover:bg-bottom ${pathname === tab.pageName ? "justify-center flex text-center bg-gradient-to-b from-black/80 bg-[length:50%_2px] bg-no-repeat bg-bottom" : "justify-center flex text-center"} `}
+            >
+              {tab.tabName}
+            </h2>
+          </Link>
+        ))}
+        <div className={"flex items-center"}>
+          <ConnectButton/>
+        </div>
       </div>
     </div>
+    // </div>
   );
 }
